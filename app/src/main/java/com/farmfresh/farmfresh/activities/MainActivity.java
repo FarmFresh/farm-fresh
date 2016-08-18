353d29a773e521bdf1eeb001008543aef3b87a22
pbabu <prabhu.babu@elementum.com>
2016-08-18 09:31:42 -0700

    added google firebase login support

--- app/src/main/java/com/farmfresh/farmfresh/activities/MainActivity.java
+++ app/src/main/java/com/farmfresh/farmfresh/activities/MainActivity.java
@@ -1,7 +1,9 @@
 package com.farmfresh.farmfresh.activities;
 
+import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
@@ -9,26 +11,62 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 import com.farmfresh.farmfresh.R;
 import com.farmfresh.farmfresh.fragments.TestFragment;
+import com.farmfresh.farmfresh.utils.Constants;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements
+        GoogleApiClient.OnConnectionFailedListener, View.OnClickListener {
 
+    private final static String TAG = MainActivity.class.getSimpleName();
     private DrawerLayout mDrawer;
     private NavigationView mNvView;
     private ActionBarDrawerToggle mDrawerToggle;
     private Toolbar mToolbar;
+    private GoogleApiClient mGoogleApiClient;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mToolbar = (Toolbar)findViewById(R.id.toolbar);
-        mDrawer = (DrawerLayout)findViewById(R.id.layout_main);
-        mNvView = (NavigationView)findViewById(R.id.nvView);
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                final FirebaseUser currentUser = firebaseAuth.getCurrentUser();
+                if(currentUser != null) {
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + currentUser.getDisplayName());
+                }else {
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+            }
+        };
+
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        mDrawer = (DrawerLayout) findViewById(R.id.layout_main);
+        mNvView = (NavigationView) findViewById(R.id.nvView);
+        mNvView.setItemIconTintList(null);
 
         //setup toolbar as action bar
         setSupportActionBar(mToolbar);
@@ -38,10 +76,93 @@ public class MainActivity extends AppCompatActivity {
 
         mDrawerToggle = setupDrawerToggle();
         mDrawer.addDrawerListener(mDrawerToggle);
+        //setup google sign in
+        setupGoogleSignIn();
 
     }
 
-    private void setupDrawerContent(){
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(mAuthListener != null){
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        // Pass any configuration change to the drawer toggles
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == Constants.RC_GOOGLE_SIGN_IN) {
+            GoogleSignInResult googleSignInResult = Auth.GoogleSignInApi
+                    .getSignInResultFromIntent(data);
+            if(googleSignInResult.isSuccess()) {
+                final GoogleSignInAccount googleSignInAccount = googleSignInResult.getSignInAccount();
+                firebaseAuthWithGoogle(googleSignInAccount);
+            }
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.googleSignInButton:
+                googleSignIn();
+                break;
+        }
+    }
+
+    private void setupGoogleSignIn() {
+        // Configure Google Sign In
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+    }
+
+    private void googleSignIn() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, Constants.RC_GOOGLE_SIGN_IN);
+    }
+
+
+    private void setupDrawerContent() {
         mNvView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(MenuItem item) {
@@ -51,11 +172,12 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    private void selectDrawerItem(MenuItem item){
+    private void selectDrawerItem(MenuItem item) {
         String title;
         switch (item.getItemId()) {
             case R.id.googleLogin:
                 title = "Google Login";
+                googleSignIn();
                 break;
             case R.id.other_1:
                 title = "Other-1";
@@ -79,28 +201,29 @@ public class MainActivity extends AppCompatActivity {
         mDrawer.closeDrawers();
     }
 
-    private ActionBarDrawerToggle setupDrawerToggle(){
-        return  new ActionBarDrawerToggle(this, mDrawer, mToolbar, R.string.drawer_open, R.string.drawer_close);
+    private ActionBarDrawerToggle setupDrawerToggle() {
+        return new ActionBarDrawerToggle(this, mDrawer, mToolbar, R.string.drawer_open, R.string.drawer_close);
     }
 
-    @Override
-    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-        mDrawerToggle.syncState();
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct){
+        Log.d(TAG, "firebaseAuthWithGoogle:" + acct.getId());
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(!task.isSuccessful()){
+                    Log.w(TAG, "signInWithCredential", task.getException());
+                    //if login fails with firebase, show a toast message
+                    Toast.makeText(MainActivity.this,
+                            "Firebase authentication failed",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
-    @Override
-    public void onConfigurationChanged(Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-        // Pass any configuration change to the drawer toggles
-        mDrawerToggle.onConfigurationChanged(newConfig);
+    private void firebaseLogout() {
+        mAuth.signOut();
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if(mDrawerToggle.onOptionsItemSelected(item)){
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
 }
---
+++ app/src/main/java/com/farmfresh/farmfresh/utils/Constants.java
@@ -0,0 +1,8 @@
+package com.farmfresh.farmfresh.utils;
+
+/**
+ * Created by pbabu on 8/18/16.
+ */
+public class Constants {
+    public static final int RC_GOOGLE_SIGN_IN = 10001;
+}
